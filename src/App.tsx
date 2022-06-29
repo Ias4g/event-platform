95fbf04e732d61ddd24de71191e6e4302fbe1495
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-29 16:08:31 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,19 @@
+import { gql, useQuery } from "@apollo/client"
+
+const GET_LESSONS_QUERY = gql`
+  query{
+    lessons{
+      id
+      <title></title>
+    }
+  }
+`
+
 function App() {
+  const { data } = useQuery(GET_LESSONS_QUERY)
+
+  console.log(data)
+
   return (
     <h1 className="text-3xl">
       Hello Iza
