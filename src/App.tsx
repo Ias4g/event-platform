90b90aca62683b9cd435e68dfb9c2b5943dd122f
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-30 15:27:19 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Header } from "./components/Header"
+import { Lesson } from "./components/Lesson"
+import { Sidebar } from "./components/Sidebar"
+import { Video } from "./components/Video"
+
 function App() {
 
   return (
-    <h1>Izael silva</h1>
+    <>
+      <Header />
+      <Sidebar />
+      <Video />
+      <Lesson />
+    </>
   )
 }
 
