3a77b26617d1b8cff4b0ca35a4fa49b5a36ef6ed
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-30 16:16:04 -0300

    Update Event.tsx

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,7 +1,17 @@
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
+import { Video } from "../components/Video";
 
 export function Event() {
     return (
-        <Header />
+        <>
+            <Header />
+            <main
+                className="flex"
+            >
+                <Video />
+                <Sidebar />
+            </main>
+        </>
     )
 }
