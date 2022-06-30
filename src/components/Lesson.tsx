2b715257487487012c7c8ed2410183c2a1716755
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-30 17:11:50 -0300

    Update Lesson.tsx

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,29 +1,42 @@
-import { CheckCircle } from 'phosphor-react'
+import { CheckCircle, Lock } from 'phosphor-react'
 
 interface LessonProps {
     title: string
     slug: string
-    availabeAt: Date
+    availableAt: Date
     type: "live" | 'class'
 }
 
 export function Lesson(props: LessonProps) {
+    const isLessonAvailable = false
+
     return (
         <a href="">
             <span className="text-gray-300">
-                {props.availabeAt.toString()}
+                {props.availableAt.toString()}
             </span>
 
             <div className="rounded border border-gray-500 p-4 mt-4">
                 <header className="flex items-center justify-between">
-                    <span
-                        className="text-sm text-blue-500 font-medium flex items-center gap-2"
-                    >
-                        <CheckCircle
-                            size={20}
-                        />
-                        Conteúdo liberado
-                    </span>
+                    {isLessonAvailable ? (
+                        <span
+                            className="text-sm text-blue-500 font-medium flex items-center gap-2"
+                        >
+                            <CheckCircle
+                                size={20}
+                            />
+                            Conteúdo liberado
+                        </span>
+                    ) : (
+                        <span
+                            className="text-sm text-orange-500 font-medium flex items-center gap-2"
+                        >
+                            <Lock
+                                size={20}
+                            />
+                            Em breve
+                        </span>
+                    )}
                     <span
                         className="text-xs rounded py-[0.125rem] px-2 text-white border border-gray-300 font-bold"
                     >
