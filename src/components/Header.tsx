7730fd4b83b6bb96d931d3c52db42b3f17aa32a5
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-30 16:05:27 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,20 @@
 import { Logo } from "./Logo";
+import { Sidebar } from "./Sidebar";
+import { Video } from "./Video";
 
 export function Header() {
     return (
-        <header
-            className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600"
-        >
-            <Logo />
-        </header>
+        <>
+            <header
+                className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600"
+            >
+                <Logo />
+            </header>
+
+            <main>
+                <Video />
+                <Sidebar />
+            </main>
+        </>
     )
 }
