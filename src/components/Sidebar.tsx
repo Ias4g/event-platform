e5f042ea5cdf3064738bcb2ca25ed51a779a8533
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-30 16:13:25 -0300

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,9 @@
 export function Sidebar() {
     return (
-        <div
+        <aside
             className="w-[348px] bg-gray-700 p-6 border-l border-gray-600"
         >
             Sidebar
-        </div>
+        </aside>
     )
 }
