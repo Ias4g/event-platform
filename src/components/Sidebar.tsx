778e399eecc815be24e9a925ba8971ea9629217b
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-30 16:21:55 -0300

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,11 @@ export function Sidebar() {
         <aside
             className="w-[348px] bg-gray-700 p-6 border-l border-gray-600"
         >
-            Sidebar
+            <span
+                className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block"
+            >
+                Cronograma de aulas
+            </span>
         </aside>
     )
 }
