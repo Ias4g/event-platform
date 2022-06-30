e26a2794cc18dbc00291514f4b0155cee0eb3530
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-30 15:18:31 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,7 @@
-import { gql, useQuery } from "@apollo/client"
-
-const GET_LESSONS_QUERY = gql`
-  query{
-    lessons{
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string
-  title: string
-}
-
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
-
-  console.log(data)
 
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <h1>Izael silva</h1>
   )
 }
 
