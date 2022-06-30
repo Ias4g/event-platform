56dd9b08da9eb6c75f944e7bf235ca9aa8bf6dd6
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-30 16:21:52 -0300

    Update Event.tsx

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -5,7 +5,7 @@ import { Video } from "../components/Video";
 export function Event() {
     return (
         <div
-            className="fles flex-col min-h-screen"
+            className="flex flex-col min-h-screen"
         >
             <Header />
             <main
