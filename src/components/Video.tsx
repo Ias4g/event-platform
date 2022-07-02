faefa2537681f064a625e740ef59bbdcf790a018
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-07-02 19:32:27 -0300

    Update Video.tsx

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,10 +1,24 @@
 export function Video() {
     return (
         <div className="flex-1"        >
-            <div className="bg-black">
+            <div className="bg-black justify-center">
                 <div
-                    className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+                    className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"
+                >
 
+                </div>
+            </div>
+
+            <div className="p-8 max-w-[1100px] mx-auto">
+                <div className="flex items-start gap-16">
+                    <div className="flex-1">
+                        <h1>
+                            Aula 01 - Abertura do Ignite Lab
+                        </h1>
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et voluptas officiis facilis quisquam obcaecati ipsa repellat aliquam similique facere animi tenetur fuga vero deserunt ducimus alias placeat, rem, reiciendis voluptatibus.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
