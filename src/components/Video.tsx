ddd6eb7846252844f2ceae38bd88e7d800c0f6ba
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-07-02 19:47:37 -0300

    Update Video.tsx

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -23,13 +23,13 @@ export function Video() {
                     </div>
                     <div className="flex flex-col gap-4">
                         <a href=""
-                            className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center"
+                            className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors"
                         >
                             <DiscordLogo size={24} />
                             Comunidade do Discord
                         </a>
                         <a href=""
-                            className="p-4 text-sm border border-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center"
+                            className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center"
                         >
                             <Lightning size={24} />
                             Acesse o desafio
