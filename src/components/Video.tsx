72254423f20db03b0149cd163337a555e494b0d1
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-07-02 19:49:27 -0300

    Update Video.tsx

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -29,7 +29,7 @@ export function Video() {
                             Comunidade do Discord
                         </a>
                         <a href=""
-                            className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center"
+                            className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors"
                         >
                             <Lightning size={24} />
                             Acesse o desafio
