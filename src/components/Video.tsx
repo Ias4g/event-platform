9e30faf230bbb1c1da647a6e1952021c51c6ea8a
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-07-02 19:36:09 -0300

    Update Video.tsx

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -12,12 +12,15 @@ export function Video() {
             <div className="p-8 max-w-[1100px] mx-auto">
                 <div className="flex items-start gap-16">
                     <div className="flex-1">
-                        <h1>
+                        <h1 className="text-2xl font-bold">
                             Aula 01 - Abertura do Ignite Lab
                         </h1>
-                        <p>
+                        <p className="mt-4 text-gray-200">
                             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et voluptas officiis facilis quisquam obcaecati ipsa repellat aliquam similique facere animi tenetur fuga vero deserunt ducimus alias placeat, rem, reiciendis voluptatibus.
                         </p>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                        
                     </div>
                 </div>
             </div>
