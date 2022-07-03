6e7ab9ac4ff4f411b52445e40a95d423ad43fd2f
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-07-03 17:10:40 -0300

    Update apollo.ts

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    // uri: 'https://api-us-east-1.graphcms.com/v2/cl4zxagst0nr701ukch3y1eju/master',
     uri: 'https://api-sa-east-1.graphcms.com/v2/cl4zzc3370ojy01ujeoev3gq8/master',
     cache: new InMemoryCache()
 }) 
