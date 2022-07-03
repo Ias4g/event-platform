4d804043e4948de2e4716c40ac76c67d9b9961b0
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-07-03 20:05:02 -0300

    Update Subscribe.tsx

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,5 +1,21 @@
+import { Logo } from "../components/Logo";
+
 export function Subscribe() {
     return (
-        <h1>Subscribe</h1>
+        <div
+            className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center"
+        >
+            <div className="max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+                <div className="max-w-[640px]">
+                    <Logo />
+                    <h1>
+                        Construa uma aplicação completa, do zero, com Raect JS.
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi illo suscipit, sed, blanditiis at dolor quos ut harum optio voluptatem, dignissimos dolorem corporis molestiae enim eligendi ea consequuntur sint fugiat.
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
