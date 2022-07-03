fcdbd25bc58c8c3232eb08c6f91ca527e8020c26
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-07-03 20:22:59 -0300

    Update Subscribe.tsx

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -5,17 +5,39 @@ export function Subscribe() {
         <div
             className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center"
         >
-            <div className="max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+            <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
                 <div className="max-w-[640px]">
                     <Logo />
-                    <h1>
-                        Construa uma aplicação completa, do zero, com Raect JS.
+                    <h1 className="mt-8 text-[2.5rem] leading-tight">
+                        Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React</strong>.
                     </h1>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi illo suscipit, sed, blanditiis at dolor quos ut harum optio voluptatem, dignissimos dolorem corporis molestiae enim eligendi ea consequuntur sint fugiat.
+                    <p className="mt-4 text-gray-200 leading-relaxed">
+                        Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
                     </p>
                 </div>
+
+                <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+                    <strong className="text-2xl mb-6 block">
+                        Inscreva-se gratuitamente
+                    </strong>
+                    <form action="" className="flex flex-col gap-2 w-full">
+                        <input
+                            id="name"
+                            type="text"
+                            name="name"
+                            placeholder="Seu nome completo"
+                        />
+                        <input
+                            id="email"
+                            type="email"
+                            name="email"
+                            placeholder="Digite seu email"
+                        />
+                    </form>
+                </div>
             </div>
+
+            <img src="/src/assets/code-mackup.png" alt="Imagem de fundo reactjs" />
         </div>
     )
 }
