1076284e2bfa65970264ce2228b512ea3668b303
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-07-02 21:26:49 -0300

    Update Event.tsx

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,8 +1,11 @@
+import { useParams } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
 
 export function Event() {
+
+    const { slug } = useParams<{ slug: string }>()
     return (
         <div
             className="flex flex-col min-h-screen"
@@ -11,7 +14,9 @@ export function Event() {
             <main
                 className="flex flex-1"
             >
-                <Video />
+                {
+                    slug ? <Video /> : <div className="flex-1" />
+                }
                 <Sidebar />
             </main>
         </div>
